bd7acd998c17f7ff4d1dae4373d13763d63d6a75
kiety <53269602+kiety@users.noreply.github.com>
2024-06-13 14:42:44 -0600

    Added song lyric message constants

---
+++ script.js
@@ -0,0 +1,14 @@
+const mess1 = "Falling away from me";
+const mess2 = "And they're like, 'it's better than yours'";
+const mess3 = "So brown eyes, I hold you near";
+const mess4 = "I tried my best to leave this all on your machine";
+const mess5 = "I consider it a challenge before the human race";
+const mess6 = "I didn't mean to cause a big scene, just give me an hour and then";
+const mess7 = "And I was like, momma, this air bubble right here, it's gonna make me fly";
+const mess8 = "So I met up with some friends at the edge of the night";
+const mess9 = "Sometimes you got to grab your world with your own two hands";
+const mess10 = "Catching things and eating their insides";
+
+let randNum = Math.floor(Math.random() * 10) + 1;
+
+console.log(randNum);
